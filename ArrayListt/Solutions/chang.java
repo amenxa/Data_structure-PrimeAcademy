9d6f780abfc10ebc93ce9940703f5772a405eef6
Ameen A shalabi <116107964+amenxa@users.noreply.github.com>
2023-08-11 18:20:10 +0300

    Update and rename shuffle.java to chang.java

--- ArrayListt/Solutions/chang.java
+++ ArrayListt/Solutions/chang.java
@@ -1,3 +1,6 @@
+*/
+: Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn]. Return the array in the form [x1,y1,x2,y2,...,xn,yn]  */
+        
 public static ArrayList<Integer> shuffle(ArrayList<Integer> nums, int n) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
 
